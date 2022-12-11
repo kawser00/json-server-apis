0675af9e095cd08025db06081822d9c813e7a52a
kawser00 <kawsarmahmud358@gmail.com>
2022-12-11 11:56:03 +0600

    [ADD] json server apis

---
+++ db.js
@@ -0,0 +1,12 @@
+const todos = require("./data/todos");
+const videos = require("./data/videos");
+const tags = require("./data/tags");
+const transactions = require("./data/transactions");
+
+module.exports = {
+  todos,
+  videos,
+  transactions,
+  tags,
+}
+
